860b13a428f12600646723e1af9b7aeeddb65001
Jaimerod <jaime.rodriguez.rafael@gmail.com>
2025-10-11 10:06:37 +0200

    Creación página login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('mi_Proyecto');
+
 }
---
+++ src/app/login/login.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [ ReactiveFormsModule, NzButtonModule, NzCheckboxModule, NzFormModule, NzInputModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  validateForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.validateForm = this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('Form Value:', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        control.markAsDirty();
+        control.updateValueAndValidity();
+      });
+    }
+  }
+}
